cb9f248769e6153fe359b88b7c8ae4ad8c37c70c
brotherjing <brotherjing@sjtu.edu.cn>
2015-10-02 23:48:35 +0800

    first commit

---
+++ app/src/main/java/com/brotherjing/aademo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.brotherjing.aademo;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends ActionBarActivity {
+
+    @ViewById(R.id.et)
+    EditText et;
+
+    @ViewById(R.id.tv)
+    TextView tv;
+
+    @Click(R.id.btn)
+    void btnClick(){
+        tv.setText(et.getText().toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
